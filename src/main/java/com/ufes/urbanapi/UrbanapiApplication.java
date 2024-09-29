e736db189f69ae1e44d1316bf01493ec96c7b6a4
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2024-09-29 05:23:07 -0300

    initial commit

---
+++ src/main/java/com/ufes/urbanapi/UrbanapiApplication.java
@@ -0,0 +1,13 @@
+package com.ufes.urbanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufes/urbanapi/UrbanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufes.urbanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
